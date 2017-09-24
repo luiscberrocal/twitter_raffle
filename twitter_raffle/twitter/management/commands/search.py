9579078f21fda2d329278436a8c978137341a9e7
luiscberrocal <luis.berrocal.1942@gmail.com>
2017-09-24 17:03:32 -0500

    Eliminated repeated code from search management.

--- twitter_raffle/twitter/management/commands/search.py
+++ twitter_raffle/twitter/management/commands/search.py
@@ -1,3 +1,4 @@
+from twitter_raffle.twitter.utils import SearchTwitterUtil
 from ..base import TweepyCommand, TweetAdapter
 from ...models import Tweet
 
@@ -40,41 +41,6 @@ class Command(TweepyCommand):
         #                 )
 
     def handle(self, **options):
-        adapter = TweetAdapter()
+        tweet_util = SearchTwitterUtil(stdout=self.stdout)
         search_query = options['search_query']
-        tweets_per_qry = 100
-        max_id = -1
-        since_id = None
-        max_tweets = 2000
-
-        tweet_count = 0
-        count = 1
-        search_params = dict()
-        search_params['q'] = search_query
-        search_params['count'] = tweets_per_qry
-
-        while tweet_count < max_tweets:
-            if max_id <= 0:
-                if since_id is not None:
-                    search_params['max_id'] = str(max_id - 1)
-                    search_params['since_id'] = since_id
-            else:
-                if since_id is None:
-                    search_params['max_id'] = str(max_id - 1)
-                else:
-                    search_params['max_id'] = str(max_id - 1)
-                    search_params['since_id'] = since_id
-
-            new_tweets = self.api.search(**search_params)
-            #serialize_data(new_tweets, format='pickle')
-            if not new_tweets:
-                self.stdout.write("No more tweets found")
-                break
-            for tweet in new_tweets:
-                tweet_data = adapter.convert(tweet)
-                data = Tweet.objects.create_from_tweet_data(tweet_data)
-                if data['tweet_created']:
-                    self.stdout.write('{} - {} - {}'.format(count, data['tweet'].created_at, data['tweet'].text))
-                    count += 1
-            tweet_count += len(new_tweets)
-            max_id = new_tweets[-1].id
+        tweet_util.search(search_query)
